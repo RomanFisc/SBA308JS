775c73cd9339180e9325d6192dca43b877bd5660
RomanFisc <romanfisc@gmail.com>
2024-05-30 11:00:17 -0400

    Data creation

---
+++ sba308.js
@@ -0,0 +1,51 @@
+// SBA 308
+
+//creating Data: 
+
+const CourseInfo = [
+  {
+    "id": 123,
+    "name": "JS Fundamentals",
+  }  
+]
+
+const AssigmentGroup = [
+  {
+    "id": 1111,
+    "name": "Function Fun",
+    // the ID of the course the assignment group belongs to
+    "course_id": 123,
+    // the percentage weight of the entire assignment group
+    "group_weight": 25,
+    "assignments": [
+      {
+        id: 1,
+        name: "Create a function",
+        due_at: "2024-02-06",
+        points_possible: 100
+      }
+    ],
+  }
+]
+
+const AssigmnetInfo = [
+  {
+    "id": number,
+    "name": string,
+    // the due date for the assignment
+    "due_at": Date string,
+    // the maximum points possible for the assignment
+    "points_possible": number,
+  }
+]
+
+const LearnerSubmission = [
+  {
+    "learner_id": number,
+    "assignment_id": number,
+    "submission": {
+      "submitted_at": Date string,
+      "score": number
+    }
+  }
+]
