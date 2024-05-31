1ff27898c1b7dd56ecb60eebd38790720f05f4e1
RomanFisc <romanfisc@gmail.com>
2024-05-31 09:45:26 -0400

    outputting student grade and assignment info

--- sba308.js
+++ sba308.js
@@ -29,6 +29,18 @@ const AssignmentGroup = [
         name: "arrays and loops",
         due_at: "2024-06-05",
         points_possible: 50
+      }, 
+      {
+        id: 3, 
+        name: "Arrow Functions",
+        due_at: "2024-04-20",
+        points_possible: 100
+      },
+      {
+        id: 4, 
+        name: "Arrow Functions",
+        due_at: "2024-05-20",
+        points_possible: 60
       }
     ],
   }
@@ -45,10 +57,26 @@ const LearnerSubmission = [
   }, 
   {
     "learner_id": 443,
+    "assignment_id": 3,
+    "submission": {
+      "submitted_at": "2024-04-12",
+      "score": 79
+    }
+  },
+  {
+    "learner_id": 443,
     "assignment_id": 2,
     "submission": {
-      "submitted_at": "2024-05-30",
+      "submitted_at": "2024-06-02",
       "score": 0
+    }
+  },
+  {
+    "learner_id": 443,
+    "assignment_id": 4,
+    "submission": {
+      "submitted_at": "2024-05-22",
+      "score": 55
     }
   }
 ]
@@ -75,19 +103,68 @@ const LearnerSubmission = [
 //    - let result = userscrore/assignment score
 //    - 
 
-function getLearnerData(/*CourseInfo, AssignmentGroup,*/ LearnerSubmission) {
+function getLearnerData(/*CourseInfo,*/ AssignmentGroup, LearnerSubmission) {
   let learnerResults = {};
+  
+  //adding learner_id to learnerResults
   LearnerSubmission.forEach(submission => {
     let userid = submission["learner_id"]
-    learnerResults["learner_id"] = userid
-  });
-  let userscore = 0;
-  let assignmentscore = 0;
-  
+    if (!learnerResults[userid]) {
+      learnerResults[userid] = {
+        totalScore: 0,
+        totalPossible: 0,
+        assignments: []
+      }
+    }
+  })
 
-  console.log(learnerResults)
+  for (let i = 0; i < AssignmentGroup.length; i++) {
+    for (let y = 0; y < AssignmentGroup[i].assignments.length; y++) {
+      let currentAssignment = AssignmentGroup[i].assignments[y];
+      if (currentAssignment.due_at < "2024-06-03") {
+        let pointsPossible = currentAssignment.points_possible;
+        let totalGrade = 0;
+
+        for (let x = 0; x < LearnerSubmission.length; x++) {
+          let currentSubmission = LearnerSubmission[x];
+          if (currentSubmission.assignment_id === currentAssignment.id) {
+            let studentScore = currentSubmission.submission.score;
+            if (currentSubmission.submission.submitted_at > currentAssignment.due_at){
+              studentScore *= 0.9;
+            }
+
+            totalGrade += pointsPossible;
+
+            let learner_id = currentSubmission.learner_id;
+
+            if (learnerResults[learner_id]) {
+              learnerResults[learner_id].totalScore += studentScore;
+
+              learnerResults[learner_id].assignments.push({
+                assignment_id: currentAssignment.id,
+                grade : (studentScore / pointsPossible) * 100
+              });
+            }
+          }
+        }
+
+        for (let learner_id in learnerResults) {
+          learnerResults[learner_id].totalPossible += totalGrade;
+        }
+      }
+    }
+    for (let learner_id in learnerResults) {
+      learnerResults[learner_id]["Student Grade"] = (learnerResults[learner_id].totalScore / learnerResults[learner_id].totalPossible) * 100;
+    }
+  }
+
+   console.dir(learnerResults, { depth: null });
 }
 
 
 
-getLearnerData(LearnerSubmission);
+getLearnerData(/*CourseInfo,*/ AssignmentGroup, LearnerSubmission);
+
+
+
+
